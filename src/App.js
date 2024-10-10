d82073ad341c34babfaec3f6d68a9a838f4d876f
firas hamila <firas.hm.1996@gmail.com>
2024-10-10 10:56:07 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <ExpenseItem />
+    </div>
+  );
 }
 
 export default App;
