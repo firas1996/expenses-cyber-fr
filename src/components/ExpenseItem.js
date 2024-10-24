28dc7489f4b0f5a688c8ca7697fc47aa256e7e64
firas hamila <firas.hm.1996@gmail.com>
2024-10-24 10:17:56 +0200

    c

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,11 @@ import React from "react";
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>month</div>
+        <div>year</div>
+        <div>day</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">€ {props.price}</div>
