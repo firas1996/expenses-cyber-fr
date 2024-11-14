28be5f2fd8545d7ba6231013989f82275f8d1711
firas hamila <firas.hm.1996@gmail.com>
2024-11-14 09:33:21 +0100

    e

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import "./ExpenseItem.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 function ExpenseItem(props) {
   const month = props.date.toLocaleString("fr-FR", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString("fr-FR", { day: "2-digit" });
   // const day = props.date.getDay();
-  let title = props.title;
-  const handelClick = () => {
-    console.log("before: ", title);
-    title = "New Title";
-    console.log("after: ", title);
-  };
+  // let title = props.title;
+  // const [title, setTitle] = useState(props.title);
+  // const handelClick = () => {
+  //   console.log("before: ", title);
+  //   setTitle("New Title");
+  //   console.log("after: ", title);
+  // };
   return (
     <div className="expense-item">
       <div className="expense-date">
@@ -21,8 +22,8 @@ function ExpenseItem(props) {
         <div className="expense-date__day">{day}</div>
       </div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <button onClick={handelClick}>Change Title!</button>
+        <h2>{props.title}</h2>
+        {/* <button onClick={handelClick}>Change Title!</button> */}
         <div className="expense-item__price">€ {props.price}</div>
       </div>
     </div>
