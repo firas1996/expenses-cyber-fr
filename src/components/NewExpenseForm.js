355baf7261874005b71dc1bb28fa755b083730d3
firas hamila <firas.hm.1996@gmail.com>
2024-11-14 11:21:01 +0100

    y

--- src/components/NewExpenseForm.js
+++ src/components/NewExpenseForm.js
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpenseForm.css";
 
 const NewExpenseForm = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [date, setDate] = useState("");
+  const handelTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handelPriceChange = (event) => {
+    setPrice(event.target.value);
+  };
+  const handelDateChange = (event) => {
+    setDate(event.target.value);
+  };
   return (
     <div className="new-expense">
       <form>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input placeholder="Title" />
+            <input
+              placeholder="Title"
+              onChange={handelTitleChange}
+              value={title}
+            />
           </div>
           <div className="new-expense__control">
             <label>Price</label>
-            <input placeholder="Price" type="number" min="0.01" step="0.01" />
+            <input
+              placeholder="Price"
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={handelPriceChange}
+              value={price}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2022-01-01" max="2025-12-31" />
+            <input
+              type="date"
+              min="2022-01-01"
+              max="2025-12-31"
+              onChange={handelDateChange}
+              value={date}
+            />
           </div>
         </div>
         <div className="new-expense__actions">
